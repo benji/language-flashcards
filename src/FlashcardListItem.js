c3e58578ac4f3953cdbdff83e5ad76d83bd29756
Benjamin Jaton <benjaminjaton@asana.com>
2020-04-11 13:04:26 -0700

    DnD UI

--- src/FlashcardListItem.js
+++ src/FlashcardListItem.js
@@ -8,14 +8,75 @@ import InputGroup from "react-bootstrap/InputGroup";
 import { withRouter } from "react-router";
 import AppContext from "./AppContext";
 import Utils from "./Utils";
+import { useDrag, useDrop } from "react-dnd";
 
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./ListFlashcards.scss";
 
+const FLASHCARD_TYPE = "FLASHCARD";
+
 function FlashcardListItem(props) {
+  const ref = useRef(null);
+
+  const [, drop] = useDrop({
+    accept: FLASHCARD_TYPE,
+
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = props.index;
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current.getBoundingClientRect();
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      props.moveFlashcard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    }
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: FLASHCARD_TYPE, id: props.flashcardId, index: props.index },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  });
+
+  const style = isDragging ? { backgroundColor: "transparent" } : {};
+
+  drag(drop(ref));
+
   return (
     <li
+      style={style}
+      ref={ref}
       key={props.flashcardId}
       onClick={Utils.gotoFn(props, `/flashcards/${props.flashcardName}`)}
     >
--- src/ListFlashcards.js
+++ src/ListFlashcards.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Link } from "react-router-dom";
 import flash_store from "./FlashStore";
 import Button from "react-bootstrap/Button";
@@ -10,6 +10,10 @@ import AppContext from "./AppContext";
 import FlashcardListItem from "./FlashcardListItem";
 import Utils from "./Utils";
 import "./ListFlashcards.scss";
+import { DndProvider } from "react-dnd";
+import MultiBackend from "react-dnd-multi-backend";
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch"; // or any other pipeline
+import update from "immutability-helper";
 
 function ListFlashcards(props) {
   const [flashcards, setFlashcards] = useState([]);
@@ -20,7 +24,7 @@ function ListFlashcards(props) {
   useEffect(() => {
     AppContext.getCachedFlashcardsArray()
       .then(arr => {
-        setFlashcards([...arr]) // copy
+        setFlashcards([...arr]); // copy
       })
       .catch(AppContext.handleError);
   }, []); //only once
@@ -29,7 +33,7 @@ function ListFlashcards(props) {
     e.preventDefault();
 
     AppContext.findFlashCardByName(newFlashcardName)
-      .then((f) => {
+      .then(f => {
         if (f) {
           alert('Flashcard "' + newFlashcardName + '" already exist');
         } else {
@@ -37,8 +41,8 @@ function ListFlashcards(props) {
           flash_store
             .addFlashcard(userdata)
             .then(id => {
-              var flashcard = { id: id, userdata: userdata }
-              AppContext.addFlashcard(flashcard)
+              var flashcard = { id: id, userdata: userdata };
+              AppContext.addFlashcard(flashcard);
               setFlashcards([...flashcards, flashcard]);
               setNewFlashcardName("");
             })
@@ -55,40 +59,60 @@ function ListFlashcards(props) {
   }
 
   function confirmDeleteAll() {
-    console.log('show')
-    setShowDeleteAllConfirm(true)
+    console.log("show");
+    setShowDeleteAllConfirm(true);
   }
   function closeDeleteAllConfirm() {
-    console.log('hide')
-    setShowDeleteAllConfirm(false)
+    console.log("hide");
+    setShowDeleteAllConfirm(false);
   }
 
   function deleteAll() {
-    console.log("DELETE ALL!")
-    closeDeleteAllConfirm()
+    console.log("DELETE ALL!");
+    closeDeleteAllConfirm();
     flash_store
       .deleteAll()
       .then(() => {
-        AppContext.deleteAll()
+        AppContext.deleteAll();
         setFlashcards([]);
         setNewFlashcardName("");
       })
       .catch(AppContext.handleError);
   }
 
+  const moveFlashcard = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragFlashcard = flashcards[dragIndex];
+      setFlashcards(
+        update(flashcards, {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, dragFlashcard]]
+        })
+      );
+    },
+    [flashcards]
+  );
+
   return (
     <React.Fragment>
       <h3>Flashcards</h3>
 
       <div className="fcRow ListFlashcards">
-        <ul className="flashcards">
-          {flashcards.map(f => {
-            return (
-              <FlashcardListItem key={f.id} flashcardId={f.id} flashcardName={f.userdata.name} />
-            );
-          })}
-        </ul>
-
+        {" "}
+        <DndProvider backend={MultiBackend} options={HTML5toTouch}>
+          <ul className="flashcards">
+            {flashcards.map((f, index) => {
+              return (
+                <FlashcardListItem
+                  key={f.id}
+                  index={index}
+                  flashcardId={f.id}
+                  flashcardName={f.userdata.name}
+                  moveFlashcard={moveFlashcard}
+                />
+              );
+            })}
+          </ul>
+        </DndProvider>
         <div className="fcRow">
           <form onSubmit={addFlashcard}>
             <Form.Label>New Flashcard</Form.Label>
@@ -110,11 +134,15 @@ function ListFlashcards(props) {
         {!showDeleteAllConfirm && (
           <Button type="button" variant="danger" onClick={confirmDeleteAll}>
             Delete All!
-        </Button>
+          </Button>
         )}
         {showDeleteAllConfirm && (
           <>
-            <Button type="button" variant="secondary" onClick={closeDeleteAllConfirm}>
+            <Button
+              type="button"
+              variant="secondary"
+              onClick={closeDeleteAllConfirm}
+            >
               Cancel
             </Button>
             <Button type="button" variant="danger" onClick={deleteAll}>
@@ -123,7 +151,6 @@ function ListFlashcards(props) {
           </>
         )}
       </div>
-
     </React.Fragment>
   );
 }
